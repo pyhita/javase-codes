bb5d5e08abd2b865752e4a0479d8d9cf9f64dc7e
kante_yang <kante_yang@trendmicro.com>
2023-09-26 15:43:32 +0800

    java8 in action 并行数据处理

---
+++ src/main/java/com/yangtao/java8/ch07/ParallelStreams.java
@@ -0,0 +1,25 @@
+package com.yangtao.java8.ch07;
+
+/**
+ * @Author: kante_yang
+ * @Date: 2023/9/26
+ */
+
+import java.util.stream.Stream;
+
+/**
+ * 并行流处理
+ */
+public class ParallelStreams {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static long parallelSum(long n) {
+        return Stream.iterate(1L, i -> i + 1)
+                .limit(n)
+                .parallel()
+                .reduce(0L, Long::sum);
+    }
+}
